df35d918e05c9e68b21e859177c2769607c2fe26
Dominic Kavalary <128938538+DominicKavalary@users.noreply.github.com>
2024-10-17 14:19:41 -0500

    Update PingMaker3.py

--- PingMaker3.py
+++ PingMaker3.py
@@ -14,33 +14,50 @@ def getOutput(Command):
   output = output.splitlines()
   return output
 
-# write a function where ti testrs if its an error or not then if it is do add it if it isnt dont add it to targets
-
+# function to ping and return results to an array#
+def getPingInfo(Address):
+  timeOfPing = time.strftime("%D:%H:%M:%S")
+  command = "ping -c 1 " + Address
+  packetLoss = 0
+  responseTime = ""
+  output = getOutput(command)
+  time.sleep(1)
+  infoFound = False
+  for line in output:
+    if "% packet loss" in line:
+      packetLoss = line.split(', ')[2].split(" ")[0]
+    elif "bytes from" in line:
+      responseTime = line[line.find("time"):]
+  return [timeOfPing,packetLoss,responseTime]
+  
+# write a function where ti testrs if its an error or not then if it is do add it if it isnt dont add it to targets#
+def testTarget(Target):
+#look for info in the output
+  command = "ping -c 1 " + Address
+  output = getOutput(command)
+  infoFound = False
+  for line in output:
+      if "% packet loss" in line:
+        infoFound = True
+      elif "bytes from" in line:
+        infoFound = True
+  # if the input isnt found, add the address to the error file
+  if infoFound == False:
+    with open("/home/PingMaker/errors/errlist.txt", "a") as errfile:
+      errfile.write("\nNo info found for: "+Address+", check format of address")
+  # regardless, return the value of infoFound so the code knows what to do
+  return infoFound
+  
 #Ping and write thread function#
 def PingandWrite(Address):
   timeOfStart = time.time()
-  errorFileNotCreated = True
   while 1 == 1:
-# Set up variables, get ping response
-    timeOfPing = time.strftime("%D:%H:%M:%S")
-    command = "ping -c 1 " + Address
-    output = getOutput(command)
-    infoFound = False
-    packetLoss = 0
-    responseTime = ""
+# get ping response and store in array
+    pingArray = getPingInfo(Address)
     tempFileName = "/home/PingMaker/csv/"+Address+"/"+Address+".csv"
-# Find info from ping output
-    for line in output:
-      if "% packet loss" in line:
-        packetLoss = line.split(', ')[2].split(" ")[0]
-      elif "bytes from" in line:
-        infoFound = True
-        responseTime = line[line.find("time"):]
-# if info is found, wait a sec before doing another ping, then, open the target's directory and append to its data file
-    if infoFound:
-      time.sleep(1)
-      with open(tempFileName, "a") as tmp:
-        tmp.write("\n"+timeOfPing+","+packetLoss+","+responseTime)
+# open the target's directory and append to its data file
+    with open(tempFileName, "a") as tmp:
+      tmp.write("\n"+pingArray[0]+","+pingArray[1]+","+pingArray[2])
 # if it has been 6 hours since the file has been rotated, take the current temp file and change its name so you can start making a new temp file
       if int((time.time()-timeOfStart)/60/60) == 6:
         newFileileName = Address+"_"+str(timeOfPing).replace("/","_").replace(":","-")+".csv"
@@ -50,19 +67,14 @@ def PingandWrite(Address):
         with open(tempFileName, "a+") as tmpNew:
           tmpNew.write("timeofPing,packetLoss,responseTime")
 # if no info was found, create an error file. then mark the errorFileNotCreated to False so that it doesnt keep writing to the file every time. Once again, make a function to test all addresses beforehand, and for the ones that fail add their addresses to a single text file with the name including the timestamp of when the error checking happened
-    else:
-      if errorFileNotCreated:
-        with open("/home/PingMaker/errors/"+Address, "a") as errfile:
-          errfile.write("\nNo info found for: "+Address+", check format of address")
-        errorFileNotCreated = False
-      time.sleep(60) # for now do a wait of 60 seconds before it tries again, in the future make the process temrinate itself, OR make the functon to test all pings beforehand to validate targets, and remove invalid from the list of targets so you dont even have to do this here.
 
 
+####MAIN####
 ####Create Directorys#####
 subprocess.Popen("mkdir /home/PingMaker/csv", shell=True, stdout=subprocess.PIPE)
 subprocess.Popen("mkdir /home/PingMaker/errors", shell=True, stdout=subprocess.PIPE)
 time.sleep(1)
-####MAIN####
+
 # grab a list of targets from the target file
 ListofTargets = []
 with open("/home/PingMaker/PingMakerTargets.txt", "r") as targetFile:
@@ -75,9 +87,12 @@ with open("/home/PingMaker/PingMakerTargets.txt", "r") as targetFile:
     else:
 # if there is no address range, add it as a singular host
       ListofTargets.append(line.replace("\n",""))
-# for every target in the list you just made, make their target directories. this is where their csv files will be stored. Maybe add a check before this to see if every target is valid.
+# for every target in the list you just made, test them to see if they are valid targets, if so make their target directories. this is where their csv files will be stored.
 for target in ListofTargets:
-  subprocess.Popen("mkdir /home/PingMaker/csv/"+target, shell=True, stdout=subprocess.PIPE)
+  if testTarget(target):
+    subprocess.Popen("mkdir /home/PingMaker/csv/"+target, shell=True, stdout=subprocess.PIPE)
+  else:
+    ListofTargets.remove(target)
 time.sleep(2)
 # now for every target in your list, append the csv header info to the file
 for target in ListofTargets:
