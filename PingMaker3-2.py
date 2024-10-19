73f2ad6e8efea86c70e35c02158111003c09449e
Dominic Kavalary <128938538+DominicKavalary@users.noreply.github.com>
2024-10-18 21:31:27 -0500

    Update PingMaker3-2.py

--- PingMaker3-2.py
+++ PingMaker3-2.py
@@ -57,7 +57,6 @@ def getTargets():
     # if they pass the initial tests, create their directories
     if not testTargetRegex(target):
       ListOfBadTargets.append(Target)
-      
   # now remove every bad target from our list
   for Target in ListOfBadTargets:
     ListOfTargets.remove(Target)
@@ -75,10 +74,49 @@ def targetFileSetup(ListOfTargets):
     with open("/home/PingMaker/csv/"+Target+"/"+Target+".csv", "a") as TargetCSVFile:
       TargetCSVFile.write("timeofPing,packetLoss,responseTime,note")
 
+### Function to do a ping command, and return the output as an array of values. The array is: the time of ping, the packet loss, the response time, and any note outputted by the command#
+def getPingArray(Target):
+  # set up of variables and grabbing the result of the ping
+  timeOfPing = time.strftime("%D:%H:%M:%S")
+  packetLoss = "NA"
+  responseTime = "NA"
+  notes= "NA"
+  output = getOutput("ping -c 1 " + Target)
+  #parsing through results of ping to grab data
+  for line in output:
+    if "% packet loss" in line and "errors" not in line:
+      packetLoss = line.split(', ')[2].split(" ")[0]
+      if bytesFound == False:
+        responseTime = "NA"
+    elif "errors" in line:
+      packetLoss = line.split(', ')[3].split(" ")[0]
+      for line2 in output:
+        if "Host Unreachable" in line:
+          notes = "Destination Host Unreachable"     ###where im leaving off###
+    elif "bytes from" in line:
+      bytesFound = True
+      responseTime = line[line.find("time")+6:]
+### Function to be threaded. This function handles the main process of pinging and storing file data
+def PingMaker(Target):
+  # Get start an event. This event can be shared with functions, and if this event is tripped, this function will end to save resources. This will be used in the case of error handling, where it is clear pinging is a waste of reosurces.
+  event = threading.Event()
+  # grab the starting time of the function. This will be used to keep track of how long the function is running so we can do a time based log rotation
+  timeOfStart = time.time()
+  # we will set up a file name to be used by other code when appending to the file so I dont have to write the whole path over and over again#
+  tempFileName = "/home/PingMaker/csv/"+Target+"/"+Target+".csv"
+  # Setting up the while statement to always run and continuously try pinging, otherwise if the event happens it will break the loop#
+  while True:
+    # Grab the info from a the ping output function, and then check the time of how long the code has ran for
+    
 ########    ----   MAIN     ----    ####### MAYBE DO THE IF MAIN THING
 
-# Get list of targets
+# Get list of targets Make a directory and a csv file for every target in that list. then, add the header info to it
 ListOfTargets = getTargets()
+targetFileSetup(ListOfTargets)
 
-#Make a directory and a csv file for every target in that list. then, add the header info to it
-targetFileSetup
+# Start a thread per target. Each thread will ping the target and log to their own files.
+for Target in ListOfTargets:
+  PingThread = threading.Thread(target=PingMaker, args=(Target,))
+  PingThread.start()
+
+
